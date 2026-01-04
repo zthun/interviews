0d6496248ea08383f3298a6c4de19410a422c65c
Anthony Bonta <zthun.coding@gmail.com>
2026-01-04 17:22:49 -0600

    build: update fashion

--- packages/interview-react/src/app/app-avatar.tsx
+++ packages/interview-react/src/app/app-avatar.tsx
@@ -1,8 +1,12 @@
-import { ZImageSource } from "@zthun/fashion-boutique";
+import { ZImage } from "@zthun/fashion-boutique";
 import { ZSizeFixed } from "@zthun/fashion-tailor";
 
 export function ZInterviewAvatar() {
   return (
-    <ZImageSource src="/png/react-512x512.png" width={ZSizeFixed.Medium} />
+    <ZImage
+      src="/png/react-512x512.png"
+      width={ZSizeFixed.Medium}
+      fit="scale-down"
+    />
   );
 }
